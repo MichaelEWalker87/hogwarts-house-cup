27fcde385a12893d992ab1c30cb25d0ef676cd60
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-05 21:33:40 -0700

    Connect the file from Pick Player

---
+++ src/Pick_Player/Pick_Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pick_Player = () => {
+    return (
+        <section>
+            <p>pick player</p>
+        </section>
+    )
+}
+
+export default Pick_Player
