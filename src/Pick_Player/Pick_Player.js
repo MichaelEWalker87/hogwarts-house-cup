000fc3a3df13852104e7352a7ed75c306a691e66
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-06 08:41:51 -0700

    Add the env file for the API key

--- src/Pick_Player/Pick_Player.js
+++ src/Pick_Player/Pick_Player.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const PickPlayer = () => {
-  return (
-      <section>
-        <p>pick player</p>
-      <select>
-        <option value='Jame Potter'>Jame Potter</option>
-        <option value='Trevor'>Trevor</option>
-      </select>
-    </section>
-  )
+export class PickPlayer extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            selectedPlayer: [],
+            players: 2,
+        }
+    }
+
+    populateCharacters = () => {
+        
+    }
+
+    populatePlayers = () => {
+         for (let i = 0; i < this.state.players; i++) {
+            return (
+                <section>
+                    <p>Player {i + 1} select your character</p>
+                    <select>
+                        <option value='Jame Potter'>Jame Potter</option>
+                        <option value='Trevor'>Trevor</option>
+                    </select>
+                </section>
+            )
+         }
+    }
+    
+    render() {
+        return (
+            <section>
+                <p>pick player</p>
+                <select>
+                    <option value='Jame Potter'>Jame Potter</option>
+                    <option value='Trevor'>Trevor</option>
+                </select>
+                <button>
+                    Submit
+                </button>
+            </section>
+        )
+    }
 }
 
 export default PickPlayer
