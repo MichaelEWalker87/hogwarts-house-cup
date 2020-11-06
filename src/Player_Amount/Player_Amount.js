975ed3ee531a08bbe5b493a9187d7cb1936cd45b
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-05 22:35:07 -0700

    Started the file setup jsx for Player Amount

--- src/Player_Amount/Player_Amount.js
+++ src/Player_Amount/Player_Amount.js
@@ -3,7 +3,12 @@ import React from 'react'
 const PlayerAmount = () => {
     return (
         <section>
-            <p>Player Amount</p>
+            <button>Submit</button>
+            <label>Pick The Amount Of Players</label>
+            <select>
+                <option value={1} >1</option>
+                <option value={2}>2</option>
+            </select>
         </section>
     )
 }
