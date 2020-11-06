a361ccc6dc89987d31b0e9523d89b209d1cbf849
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-05 21:34:29 -0700

    Connect the file from Pick Amount

---
+++ src/Player_Amount/Player_Amount.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Player_Amount = () => {
+    return (
+        <section>
+            <p>Player Amount</p>
+        </section>
+    )
+}
+
+export default Player_Amount
+
+
