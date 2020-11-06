895ad858512a0f40000b3c43954ef392065fdae9
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-05 22:59:47 -0700

    Display the jsx for the porject

--- src/Player_Amount/Player_Amount.js
+++ src/Player_Amount/Player_Amount.js
@@ -3,11 +3,23 @@ import React from 'react'
 const PlayerAmount = () => {
  return (
   <section>
-    <button>Submit</button>
     <label>Pick The Amount Of Players</label>
     <input
-            
+      type='number'
+      name='Amount of Players Input'
+      min={2}
+      max={8}
+      value=''
+      onChange=''
     />
+    <button
+      role="button"
+      type="button"
+      className="login-submit"
+      onClick=''
+    >
+      Submit
+    </button>
   </section>
  )
 }
