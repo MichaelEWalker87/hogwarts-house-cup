e6eab77119556a6524e8717c4b0db79fccc3a1fe
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-05 21:39:22 -0700

    Connet App to the the Rules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QuickStart from './Quick_Start/Quick_Start.js'
 import Player_Amount from './Player_Amount/Player_Amount.js'
+import Rules from './Rules/Rules.js'
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class App extends Component {
         <h1>Enter in the amount</h1>
         <QuickStart /> 
         <Player_Amount />
+        <Rules />
       </section>
     )
   }
