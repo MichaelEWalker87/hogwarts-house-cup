f3dcbf518ae8740c4a3cf900f7b41658d401cd80
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-06 09:48:06 -0700

    Add the API call with the key

--- src/Api_calls/API.js
+++ src/Api_calls/API.js
@@ -1,5 +1,4 @@
 export const getCharacter = () => {
-  console.log(process.env.REACT_APP_API_KEY)
   return fetch (`https://www.potterapi.com/v1/characters?key=$2a$10$yK${process.env.REACT_APP_API_KEY}`)
   .then(respose => respose.json())
 }
