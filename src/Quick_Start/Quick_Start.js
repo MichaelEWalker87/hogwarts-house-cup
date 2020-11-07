e406542cf48174bb6ec84b36e663b20ea2816be6
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-07 09:34:33 -0700

    Add the Quick start menue buttons and text

--- src/Quick_Start/Quick_Start.js
+++ src/Quick_Start/Quick_Start.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const QuickStart = () => {
   return(
-    <p>Test Quick Start</p>
+    <section>
+      <h3>
+      Choose 'Start' and be taken directly to gameplay, 
+      or select 'Rule' to customize the gameplay experience.
+      </h3>
+      <section>
+        <label>
+          Quick Start 
+        </label>
+        <button>
+          Start
+        </button>
+      </section>
+      <section>
+        <label>
+          Add A Game Rule 
+        </label>
+        <button>
+          Rules
+        </button>
+      </section>
+    </section>
   )
 }
 export default QuickStart
