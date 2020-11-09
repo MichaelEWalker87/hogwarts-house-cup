0c6309eea3e4541d9e8b827815b084cf740c6710
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-08 21:58:34 -0700

    Add unit test to the Quickstart

--- src/Quick_Start/QuickStart.test.js
+++ src/Quick_Start/QuickStart.test.js
@@ -6,16 +6,27 @@ import userEvent from '@testing-library/user-event'
 import QuickStart from "./QuickStart.js"
 
 describe('Quickstart', () => {
+  let headerText, quickStartText, ruleText;
+  let Rulebutton, quickstartButton; 
   beforeEach(() => {
-    mockFuntion = jest.fn()
     render(
       <MemoryRouter>
         <QuickStart/>
       </MemoryRouter>
     )
+   headerText = screen.getByText(/choose 'start' and be taken directly to gameplay, or select 'rule' to customize the gameplay experience./i)
+   quickStartText = screen.getByText(/quickstart/i)
+   ruleText = screen.getByText(/add a game rule/i)
+   Rulebutton = screen.getByRole('button', { name: /rules/i })
+   quickstartButton = screen.getByRole('button', { name: /start/i })
   })
+
   it('should render Quickstart to the page', () => {
-    userEvent.selectOptions()
+    expect(headerText).toBeInTheDocument()
+    expect(quickStartText).toBeInTheDocument()
+    expect(ruleText).toBeInTheDocument()
+    expect(Rulebutton).toBeInTheDocument()
+    expect(quickstartButton).toBeInTheDocument()
   })
   
 })
