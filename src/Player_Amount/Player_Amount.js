5f789a249f120d3bb44012bfe32cf7c975101296
Michael Walker <Michael.e.walker.87@gmail.com>
2021-03-05 10:55:33 -0700

    Postion the Ticket

--- src/Player_Amount/Player_Amount.js
+++ src/Player_Amount/Player_Amount.js
@@ -20,39 +20,41 @@ export class PlayerAmount extends Component {
   render() {
     return (
         <section className="amount-section">
-          <section className="pick-player-title pick-player-title-color">
-            <span>P</span><span>i</span><span>c</span>
-            <span>k</span><span>&nbsp;</span><span>T</span>
-            <span>h</span><span>e</span><span>&nbsp;</span>
-            <span>A</span><span>m</span><span>o</span>
-            <span>u</span><span>n</span><span>t</span>
-            <span>&nbsp;</span><span>O</span><span>f</span>
-            <span>&nbsp;</span><span>P</span><span>l</span>
-            <span>a</span><span>y</span><span>e</span>
-            <span>r</span><span>s</span>
-          </section>
-        <input
-          type='number'
-          name='players'
-          min={2}
-          max={8}
-          value={this.state.players}
-          onChange={this.handleChange}
-          className="amount-picker"
-        />
-        <Link 
-          to="/select-character"
-          label="select-character"
-        >
-          <button
-            role="button"
-            disabled={this.state.players > 8 || this.state.players < 2}
-            className="login-submit"
-            onClick={() => this.props.updatePlayercount(this.state.players)}
-          >
-            Submit
-          </button>
-        </Link>
+          <section className="ticket">
+            <section className="pick-player-title pick-player-title-color">
+              <span>P</span><span>i</span><span>c</span>
+              <span>k</span><span>&nbsp;</span><span>T</span>
+              <span>h</span><span>e</span><span>&nbsp;</span>
+              <span>A</span><span>m</span><span>o</span>
+              <span>u</span><span>n</span><span>t</span>
+              <span>&nbsp;</span><span>O</span><span>f</span>
+              <span>&nbsp;</span><span>P</span><span>l</span>
+              <span>a</span><span>y</span><span>e</span>
+              <span>r</span><span>s</span>
+            </section>
+            <input
+              type='number'
+              name='players'
+              min={2}
+              max={8}
+              value={this.state.players}
+              onChange={this.handleChange}
+              className="amount-picker"
+            />
+            <Link 
+              to="/select-character"
+              label="select-character"
+            >
+              <button
+                role="button"
+                disabled={this.state.players > 8 || this.state.players < 2}
+                className="login-submit"
+                onClick={() => this.props.updatePlayercount(this.state.players)}
+              >
+                Submit
+              </button>
+            </Link>
+        </section>
       </section>
     )
   }
